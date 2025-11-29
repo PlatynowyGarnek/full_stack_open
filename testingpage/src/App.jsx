c53833b3d17f4c8e25f5fc88acabc756b59667f5
Wiktor Rusin <vitorusin123@gmail.com>
2025-11-29 13:22:19 +0100

    usage of event handlers and buttons

--- testingpage/src/App.jsx
+++ testingpage/src/App.jsx
@@ -1,26 +1,21 @@
-const Hello = ({name , age }) => {
-
-  const bornYear = () => new Date().getFullYear() - age
-  
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old.
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
+import { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+
+  const [ counter, setCounter ] = useState(0)
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  console.log('rendering...', counter)
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10} />
-      <Hello name={name} age={age} />
+    <div>{counter}</div>
+    <button onClick={() => {setCounter(counter+1); console.log('clicked')}}>plus</button>
+    <button onClick={() => {setCounter(0); console.log('clicked zero')}}>zero</button>
+
     </div>
   )
 }
