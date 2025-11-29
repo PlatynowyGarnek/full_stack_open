c5bcab376fd558f171e9c4290032cd13c0242577
Wiktor Rusin <rudowicki@MacPlaty.local>
2025-11-29 10:39:26 +0100

    finished exercise 1.5 - version: 1.0

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -11,19 +11,27 @@ const Part = (data) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [{
+      name: 'Fundamentals of React',
+      exercises: 10 
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7 
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }]
+  }
   return (
     <div>
-      <Part part= {part1} exercise = {exercises1} />
-      <Part part= {part2} exercise = {exercises2} />
-      <Part part= {part3} exercise = {exercises3} />
+      <Part part= {course.parts[0].name} exercise = {course.parts[0].exercises} />
+      <Part part= {course.parts[1].name} exercise = {course.parts[1].exercises} />
+      <Part part= {course.parts[2].name} exercise = {course.parts[2].exercises} />
+
     </div>
   )
 }
