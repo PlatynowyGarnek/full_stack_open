d5d314ff9b5a2bc5182d7ccf1d346102e74ff71e
Wiktor Rusin <vitorusin123@gmail.com>
2025-11-29 14:18:43 +0100

    child functions

--- testingpage/src/App.jsx
+++ testingpage/src/App.jsx
@@ -1,23 +1,43 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
 const App = () => {
-
   const [ counter, setCounter ] = useState(0)
 
-  const handleClick = () => {
-    console.log('clicked')
-  }
-
-  console.log('rendering...', counter)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-    <div>{counter}</div>
-    <button onClick={() => {setCounter(counter+1); console.log('clicked')}}>plus</button>
-    <button onClick={() => {setCounter(0); console.log('clicked zero')}}>zero</button>
 
+      <Display counter={counter}/>
+      <Button
+        onClick= {increaseByOne}
+        text = 'plus'
+      />
+      <Button
+        onClick= {setToZero}
+        text = 'zero'
+      />
+      <Button
+        onClick= {decreaseByOne}
+        text = 'minus'
+      />
+      
+      
     </div>
   )
 }
-
 export default App
