605aae1d362a9b869dfe8c62e3be66812e8a1647
Wiktor Rusin <vitorusin123@gmail.com>
2025-12-03 22:03:07 +0100

    1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,80 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+  const Button = ({buttonType,text}) => {
+    return (
+      <button onClick={buttonType}>{text}</button>
+    )
+  }
+
+    
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodButtonClicked = () => {
+    const newGood = good
+    return (
+      
+      setGood(newGood +1)
+    )
+  }
+
+    const neutralButtonClicked = () => {
+    const newNeutral = neutral
+    return (
+      
+      setNeutral(newNeutral +1)
+    )
+  }
+
+    const badButtonClicked = () => {
+    const newBad = bad
+    return (
+      
+      setBad(newBad +1)
+    )
+  }
+  const averageScore = () => {
+    
+    const goodScore = good * 1
+    const neutralScore = neutral * 0
+    const badScore = bad * -1
+    if (good + neutral + bad === 0) {
+      return 0
+    }
+    return (
+      (goodScore + neutralScore + badScore) / (good + neutral + bad)
+    )
+  }
+  const positiveFeedback = () => {
+    if (good + neutral + bad === 0) {
+      return "0 %"
+    }
+    const percentage = (good / (good + neutral + bad)) * 100
+    return (
+      percentage.toFixed(2) + " %"
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Give feedback</h1>
+      <Button buttonType={goodButtonClicked} text="good" />
+      <Button buttonType={neutralButtonClicked} text="neutral" />
+      <Button buttonType={badButtonClicked} text="bad" />
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {averageScore()}</p>
+      <p>positive {positiveFeedback()}</p>
+    </div>
   )
 }
 
